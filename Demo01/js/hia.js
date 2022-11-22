ae3c52ac3a0eabdcf3e717da6d728a878547d588
Dr Ric colasanti <developer.colasanti@gmail.com>
2022-11-22 12:06:50 +0000

    Remove Green space

--- Demo01/js/hia.js
+++ Demo01/js/hia.js
@@ -1,21 +1,20 @@
 var size = 50;
 var households = 40
-var maxDistance = Math.sqrt(size*size+size*size)
+var maxDistance = Math.sqrt(size * size + size * size)
 let visible_canvas = document.getElementById("cacanvas");
-visible_canvas.width=600
-visible_canvas.height=600
-console.log(visible_canvas.width);
+visible_canvas.width = 600
+visible_canvas.height = 600
 var caCanvas = new CACanvas(size);
 var places
 var population = []
+var greenspaces = []
 visible_canvas.addEventListener("click", canvasClick);
 
-       
+
 var setVals = function () {
-    console.log("here");
     households = Number(document.getElementById("numberOfHomes").value);
     document.getElementById("numberOfHomesLab").innerHTML = households
-}   
+}
 
 class LandType extends Patch {
     constructor(id) {
@@ -30,7 +29,7 @@ class Person extends Agent {
         super();
         this.color = color;
         this.health = health
-        this.id="person"
+        this.id = "person"
     }
 }
 class Shop extends Agent {
@@ -44,44 +43,60 @@ class Shop extends Agent {
     }
 
 }
- 
 
-function canvasClick(event){
-    [x,y]= caCanvas.getCell(event.clientX,event.clientY,"cacanvas");
-    land = places.getPatch(x,y)
-    if(land.type == "green"){
-        land.type="land"
-        land.color=rndGray()
-    }else{
-        land.type="green"
-        land.color="green"
-    
-        for(let i=0; i<population.length;i++){
+
+function canvasClick(event) {
+    [x, y] = caCanvas.getCell(event.clientX, event.clientY, "cacanvas");
+    land = places.getPatch(x, y)
+    if (land.type == "green") {
+        land.type = "land"
+        land.color = rndGray()
+        const index = greenspaces.indexOf(land);
+        if (index > -1) { // only splice array when item is found
+            greenspaces.splice(index, 1); // 2nd parameter means remove one item only
+        }
+    } else {
+        land.type = "green"
+        land.color = "green"
+        greenspaces.push(land)
+    }
+    healthUpdate()
+}
+
+var healthUpdate = function () {
+    for (let i = 0; i < population.length; i++) {
+        const person = population[i];
+        person.health=0
+        person.color = "red"
+    }
+    for (let i = 0; i < greenspaces.length; i++) {
+        land = greenspaces[i]
+        for (let i = 0; i < population.length; i++) {
             person = population[i]
-            let d =1-(land.getDistance(person.home)/maxDistance)
-            if(d>person.health){
+            let d = 1 - (land.getDistance(person.home) / maxDistance)
+            if (d > person.health) {
                 person.health = d
-                let col = "rgb(50,"+Math.floor(255*d)+",50)"
+                let col = "rgb(50," + Math.floor(255 * d) + ",50)"
                 person.color = col
             }
         }
     }
-    
     draw()
 }
 
 var setup = function () {
     places
     population = []
+    greenspaces = []
     places = new Patches(size);
     let id = 0
-    for(let x= 0 ; x<size; x++){
-        for(let y = 0; y<size; y++){
+    for (let x = 0; x < size; x++) {
+        for (let y = 0; y < size; y++) {
             const land = new LandType(id)
-            land.color= rndGray()
-            id+=1
+            land.color = rndGray()
+            id += 1
             places.addPatch(land)
-            if(rndInt(100)<households){
+            if (rndInt(100) < households) {
                 agent = new Person(0, "red");
                 population.push(agent)
                 land.addAgent(agent)
@@ -90,7 +105,7 @@ var setup = function () {
     }
     places.setNeighbors()
     draw()
-    
+
 }
 
 
@@ -101,7 +116,6 @@ var draw = function () {
     });
     for (let i = 0; i < population.length; i++) {
         const person = population[i];
-        console.log(person.color);
         caCanvas.drawCircle(person.xPos(), person.yPos(), person.color, "black", 4, 1);
     }
     caCanvas.update("cacanvas");
